aef5e00f26dbd62d26992a8d575028c8d3643fc2
Praneel Tejpal Bora <praneelbora@gmail.com>
2022-10-11 18:11:33 +0530

    third

---
+++ tic_tac_toe.py
@@ -0,0 +1,13 @@
+list=[[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+win=' '
+c1=0
+c2=0
+c3=0
+d1=0
+d2=0
+def check():
+    for i in {0,1,2}:
+        # for j in {0,1,2}:
+        if(list[i][0]==list[i][1]==list[i][2]):
+            pass
+    
