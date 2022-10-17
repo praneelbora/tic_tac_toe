a573033085b56465787c2c843750631c12d96e2f
Praneel Tejpal Bora <praneelbora@gmail.com>
2022-10-17 16:03:35 +0530

    Final

---
+++ tic.py
@@ -0,0 +1,106 @@
+theboard = { '1' : ' ' , '2' : ' ' , '3' : ' ' ,
+             '4' : ' ' , '5' : ' ' , '6' : ' ' ,
+             '7' : ' ' , '8' : ' ' , '9' : ' '}
+board_keys = []
+
+for key in theboard:
+    board_keys.append(key)
+
+
+def actualboard(board):
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print('-----')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-----')
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+
+def tictactoe():
+
+    turn = 'X'
+    count = 0
+
+    for i in range(10):
+        actualboard(theboard)
+        print("Now it's" + turn + "'s turn. Enter your desired position (1-9):")
+
+        move = input()
+
+        if theboard[move] == ' ':
+            theboard[move] = turn
+            count += 1
+
+        else:
+            print("That place is already filled. Please choose another one.")
+            continue
+
+        if count >= 5:
+            if theboard['1'] == theboard['2'] == theboard['3'] != ' ' :
+                print(actualboard(theboard))
+                print('\nGame over .\n')
+                print(' **** ' + turn + ' won. ****')
+                break
+
+            elif theboard['4'] == theboard['5'] == theboard['6'] != ' ' :
+                print(actualboard(theboard))
+                print('\nGame over.\n')
+                print(' **** ' + turn + ' won. ****')
+                break
+            
+            elif theboard['7'] == theboard['8'] == theboard['9'] != ' ' :
+                print(actualboard(theboard))
+                print('\nGame over.\n')
+                print(" **** " + turn + " won. ****")
+                break
+
+            elif theboard['1'] == theboard['4'] == theboard['7'] != ' ' :
+                actualboard(theboard)
+                print('\nGame over.\n')
+                print(" **** " + turn + " won. ****")
+                break
+
+            elif theboard['2'] == theboard['5'] == theboard['8'] != ' ' :
+                actualboard(theboard)
+                print('\nGame over.\n')
+                print(" **** " + turn + " won. ****")
+                break
+
+            elif theboard['3'] == theboard['6'] == theboard['9'] != ' ' :
+                print(actualboard(theboard))
+                print('\nGame over.\n')
+                print(" **** " + turn + " won. ****")
+                break
+
+            elif theboard['1'] == theboard['5'] == theboard['9'] != ' ' :
+                print(actualboard(theboard))
+                print('\nGame over.\n')
+                print(" **** " + turn + " won. ****")
+                break
+
+            elif theboard['3'] == theboard['5'] == theboard['7'] != ' ' :
+                print(actualboard(theboard))
+                print('\nGame over.\n')
+                print(" **** " + turn + " won. ****")
+                break
+
+        if count == 9:
+            print('\nGame over.\n')
+            print("It's a tie")
+
+        if turn == "X":
+            turn = 'O'
+
+        else:
+            turn = 'X'
+
+    restart = input("Do you want to play again?(y/n)")
+    if restart == 'y' or restart == 'Y':
+        for key in board_keys:
+            theboard[key] = " "
+
+        tictactoe()
+
+tictactoe()
+actualboard()
+        
+
+        
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,43 +1,60 @@
-list=[['1','2','3'],['4','5','6'],['7','8','9']]
+list=[['_','_','_'],['_','_','_'],[' ',' ',' ']]
 win='a'
-c1=0
-c2=0
-c3=0
-d1=0
-d2=0
+cnt=0
 def check():
     win=' '
     cnt=0
-    for i in {0,1,2}:
-        if(list[i][0]==list[i][1]and list[i][1]==list[i][2] and list[i][0]!=' '):
+    for i in range(3):
+        if  (list[i][0]==list[i][1]==list[i][2] and list[i][0]!=' ' and list[i][0]!='_'):   #rows
             return list[i][0]
-        elif(list[0][i]==list[1][i] and list[1][i]==list[2][i]and list[0][i]!=' '):
+        elif(list[0][i]==list[1][i]==list[2][i] and list[0][i]!=' ' and list[0][i]!='_'):   #columns
             return list[0][i]
-    if(list[0][0]==list[1][1]and list[1][1]==list[2][2] and list[1][1]!=' '):
-        return list[0][0]
-    if(list[0][2]==list[1][1]and list[1][1]==list[2][0] and list[1][1]!=' '):
-        return list[0][0]
+
+    if(list[0][0]==list[1][1]==list[2][2] and list[1][1]!=' ' and list[1][1]!='_'):  #diagonal 1
+        return list[1][1]
+    if(list[0][2]==list[1][1]==list[2][0] and list[1][1]!=' ' and list[1][1]!='_'):  #diagonal 2
+        return list[1][1]
     return -1
+
 check()
+
 def printf():
-    print("\n")
+    print()
     for i in {0,1,2}:
         for j in {0,1,2}:
             print(list[i][j],end='')
             if(j<2):
                 print("|",end='')
         print()
-    print("\n")
+    print()
             
-turn=0
+turn=1
 while(True):
     printf()
-    if (turn%2==0):
+    inp=''
+    if (turn%2==1):
         print("X plays\nWhich block do you choose? ")
         inp=int(input())
-        
-    if (turn%2==1):
+        if list[(int)((inp-1)/3)][(inp-1)%3]=='X' or list[(int)((inp-1)/3)][(inp-1)%3]=='O':
+            print("\nWrong input, please play again")
+            continue
+
+    if (turn%2==0):
         print("O plays\nWhich block do you choose? ")
         inp=int(input())
+        if list[(int)((inp-1)/3)][(inp-1)%3]=='X' or list[(int)((inp-1)/3)][(inp-1)%3]=='O':
+            print("\nWrong input, please play again")
+            continue
+
+    list[(int)((inp-1)/3)][(inp-1)%3]='X' if turn%2==1 else 'O'
+    if(check()!=-1):
+        printf()
+        print(check()," Wins\n")
+        break
+
     turn+=1
-    break
+    if turn==10:
+        printf()
+        print("\nDraw")
+        break
+    
